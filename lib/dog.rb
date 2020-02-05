115b3e7754d2aac7bb0959ab3c5be268347476db
Jordon Terry <jt5493@hotmail.com>
2020-02-05 02:44:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name= (name)
+    @dog_name = name
+    @dog_breed = breed
+  end
+
+  def name
+    @dog_name
+  end
+
+  def breed= (breed)
+    @dog_breed = breed
+  end
+
+  def breed
+    @dog_breed
+  end
+end
